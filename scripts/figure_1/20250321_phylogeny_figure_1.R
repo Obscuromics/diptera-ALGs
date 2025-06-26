b1fc2ffd78511b97ed68aff0a71279c66914f1d8
aleksandrabliznina <ab66@sanger.ac.uk>
2025-06-26 14:17:04 +0100

    Updated script for the figure 1

--- scripts/figure_1/20250321_phylogeny_figure_1.R
+++ scripts/figure_1/20250321_phylogeny_figure_1.R
@@ -8,8 +8,10 @@ library(ggplot2)
 library(dplyr)
 library(phytools)
 ################################################################################
-tree <- read.tree("C:/Users/julia/Documents/Kamil/trees/315_supermatrix.phy.treefile")
-names <- read.table("C:/Users/julia/Documents/Kamil/trees/acc_names_match.txt", sep = ",")
+#root <- "/Users/ab66/Documents/sanger_work/diptera/diptera-ALGs/"
+root <- getwd()
+tree <- read.tree(paste0(root, "data/315_supermatrix.phy.treefile"))
+names <- read.table(paste0(root, "data/acc_names_match.txt"), sep = ",")
 
 # change accessions to species names
 tip_labels <- tree$tip.label
@@ -50,53 +52,44 @@ colour_tips <- c("Myopa_testacea",
                  "Melieria_crassipennis",
                  "Eupeodes_corollae")
 
-# prepare scale (assume that flies diverged from last common ancestor 260 Mio
-# years ago)
-#max_depth <- max(nodeHeights(tree_clean))
-#scaling_factor <- 260 / max_depth
-#tree_clean$edge.length <- tree_clean$edge.length * scaling_factor
-
-# plot tree
-#tree_show <- ggtree(tree_clean) +
+tree_show <- ggtree(tree_clean)
   #geom_tree(aes(color = label %in% colour_tips), size = 0.5) +
   #geom_tiplab(aes(color = "black"), align = TRUE, offset = 0.05, 
-              #linetype = "dotted", linesize = 0.05, size = 1.2) +
+  #            linetype = "dotted", linesize = 0.05, size = 1.2) +
   #scale_color_manual(values = c("TRUE" = "red", "FALSE" = "black")) +
-  #xlim(NA, 500) +
-  #theme(legend.position = "none") +
-  #geom_treescale(x = 80, y = -10, width = 50)
-
-
-tree_show <- ggtree(tree_clean) +
-  geom_tree(aes(color = label %in% colour_tips), size = 0.5) +
-  geom_tiplab(aes(color = "black"), align = TRUE, offset = 0.05, 
-              linetype = "dotted", linesize = 0.05, size = 1.2) +
-  scale_color_manual(values = c("TRUE" = "red", "FALSE" = "black")) +
-  theme(legend.position = "none") 
-
+  #theme(legend.position = "none") 
 tree_show
 
-
 # safe image
-ggsave("tree_figure_1.png", plot=tree_show, width=10, height=13, dpi=600)
+#ggsave(paste0(root, "figures/tree_figure_1.png"), plot=tree_show, width=10, height=13, dpi=600)
+
 ################################################################################
-# plot traits
+# extract information about the genomes
+require('gsheet')
+all_genome_data <- read.csv(text = gsheet2text("https://docs.google.com/spreadsheets/d/1K01wVWkMW-m6yT9zDX8gDekp-OECubE-9HcmD8RnmkM/edit?usp=sharing", format='csv'),
+                            stringsAsFactors = F, header = T, check.names = F)
+genome_data <- all_genome_data[all_genome_data[, 'TO ADD'] == 'KEEP', ]
+
+species_family_tab <- genome_data[, c('species', 'family', 'genome_size', 'num_chrom')]
+colnames(species_family_tab) <- c("label", "family", 'genome_size', 'num_chrom')
+
 # extract correct order of species
 data <- tree_show$data
+sorted_data_tips_desc <- data %>% filter(isTip == TRUE) %>% arrange(-y)
 
-sorted_data_tips_desc <- data %>%
-  filter(isTip == TRUE) %>%
-  arrange(desc(y))
+# add family names
+sorted_data_tips_desc <- left_join(sorted_data_tips_desc, species_family_tab, by = "label")
+sorted_data_tips_desc <- sorted_data_tips_desc %>% filter(!is.na(family))
+sorted_data_tips_desc <- as.data.frame(sorted_data_tips_desc)
 
-#write.table(sorted_data_tips_desc$label, file = "labels_sorted.tsv", sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
 ################################################################################
-# plot the three traits
-details <- read.table("C:/Users/julia/Documents/Kamil/txt/20250318_master_sheet_Diptera - data_figure_1.tsv", sep = "\t", header = FALSE)
-details$group <- (cumsum(details$V2 != dplyr::lag(details$V2, default = details$V2[1])) %% 2) + 1
-head(details)
+# plot chromosome number and genome size
+
+sorted_data_tips_desc$group <- (cumsum(
+  sorted_data_tips_desc$family != dplyr::lag(sorted_data_tips_desc$family, default = sorted_data_tips_desc$family[1])) %% 2) + 1
 
 # plot haploid chromosome number
-fig1_hap <- ggplot(details, aes(x = V5, y = factor(V4, levels = rev(unique(V4))), fill = factor(group))) +
+fig1_hap <- ggplot(sorted_data_tips_desc, aes(x = num_chrom, y = factor(y, levels = rev(unique(y))), fill = factor(group))) +
   geom_bar(stat = "identity") +
   xlab("V5") +
   ylab("V4 (Reversed)") +
@@ -108,37 +101,21 @@ fig1_hap <- ggplot(details, aes(x = V5, y = factor(V4, levels = rev(unique(V4)))
         axis.text.y = element_blank(),
         axis.title.y = element_blank(),
         axis.title.x = element_blank(),
-        legend.position = "none")
+        legend.position = "none") +
+  annotate(xmin = 6, xmax = 7, )
 
-fig1_hap
-ggsave("figure_1_haploidnumber.png", plot=fig1_hap, width=2, height=13, dpi=600)
+#fig1_hap
 
-# plot BUSCO completeness
-fig1_BUS <- ggplot(details, aes(x = V7, y = factor(V4, levels = rev(unique(V4))), fill = factor(group))) +
-  geom_bar(stat = "identity") +
-  xlab("V7") +
-  ylab("V4 (Reversed)") +
-  scale_fill_manual(values = c("1" = "gray30", "2" = "gray60")) +
-  scale_x_continuous(breaks = c(0, 50, 100)) +
-  theme_minimal() +
-  theme(panel.grid = element_blank(),
-        # panel.grid.major.x = element_line(color = "gray", size = 0.5),
-        axis.text.y = element_blank(),
-        axis.title.y = element_blank(),
-        # axis.text.x = element_blank(),
-        axis.title.x = element_blank(),
-        legend.position = "none")
-fig1_BUS
-ggsave("figure_1_BUSCO_complete.png", plot=fig1_BUS, width=1, height=13, dpi=600)
+#ggsave(paste0(root, "figures/figure_1_haploidnumber.png"), plot=fig1_hap, width=2, height=13, dpi=600)
 
 # plot genome size
-fig1_size <- ggplot(details, aes(x = V6, y = factor(V4, levels = rev(unique(V4))), fill = factor(group))) +
+fig1_size <- ggplot(sorted_data_tips_desc, aes(x = genome_size, y = factor(y, levels = rev(unique(y))), fill = factor(group))) +
   geom_bar(stat = "identity") +
   xlab("V6 (Millionen)") +
   ylab("V4 (Reversed)") +
   scale_fill_manual(values = c("1" = "gray30", "2" = "gray60")) +
   scale_x_continuous(
-    breaks = seq(0, max(details$V6, na.rm = TRUE), by = 500e6),
+    breaks = seq(0, max(sorted_data_tips_desc$genome_size, na.rm = TRUE), by = 500e6),
     labels = scales::label_number(scale = 1e-6)
   ) +
   theme_minimal() +
@@ -149,49 +126,52 @@ fig1_size <- ggplot(details, aes(x = V6, y = factor(V4, levels = rev(unique(V4))
         # axis.text.x = element_blank(),
         axis.title.x = element_blank(),
         legend.position = "none")
-fig1_size
-ggsave("figure_1_size.png", plot=fig1_size, width=2, height=13, dpi=600)
-################################################################################
-# plot BUSCO completeness for removed families
-# for supplementary figures
-removed_genomes <- read.table("C:/Users/julia/Documents/Kamil/txt/removed.txt", 
-                              header = FALSE, sep = "\t", stringsAsFactors = FALSE)
-head(removed_genomes)
-details <- read.table("C:/Users/julia/Documents/Kamil/txt/20250318_master_sheet_Diptera - data_figure_1.tsv", sep = "\t", header = FALSE)
-details$group <- (cumsum(details$V2 != dplyr::lag(details$V2, default = details$V2[1])) %% 2) + 1
-head(details)
 
-details$V9_clean <- sub("\\.\\d+$", "", details$V9)
-filtered_details <- details[details$V9_clean %in% removed_genomes$V1, ]
-filtered_details <- filtered_details[match(removed_genomes$V1, filtered_details$V9_clean), ]
-head(filtered_details)
+#fig1_size
 
-fig1_BUS_rem <- ggplot(filtered_details, aes(x = V7, y = factor(V4, levels = rev(unique(V4))))) +
-  geom_bar(stat = "identity") +
-  xlab("V7") +
-  ylab("V4 (Reversed)") +
-  #scale_fill_manual(values = c("1" = "gray30", "2" = "gray60")) +
-  scale_x_continuous(breaks = c(0, 50, 100)) +
-  theme_minimal() +
-  theme(panel.grid = element_blank(),
-        # panel.grid.major.x = element_line(color = "gray", size = 0.5),
-        axis.text.y = element_blank(),
-        axis.title.y = element_blank(),
-        # axis.text.x = element_blank(),
-        axis.title.x = element_blank(),
-        legend.position = "none",
-        axis.text.x = element_text(size = 36))
-fig1_BUS_rem
-ggsave("figure_1_BUSCO_complete_rem.png", plot=fig1_BUS_rem, width=3, height=20, dpi=600)
+#ggsave(paste0(root, "figures/figure_1_size.png"), plot=fig1_size, width=2, height=13, dpi=600)
+
 ################################################################################
 
+# arrange three plots together
+plt_all <- ggarrange(tree_show + fig1_size + fig1_hap, nrow = 1)
+ggsave(paste0(root, "figures/fig1_first_three.png"), plot=plt_all, dpi=600)
+ggsave(paste0(root, "figures/fig1_first_three.svg"), plot=plt_all, dpi=600)
+
+################################################################################
+# plot species to family connector
+
+# reverse the order
+sorted_data_tips_desc <- sorted_data_tips_desc %>% arrange(y)
+
+number_of_species <- nrow(sorted_data_tips_desc)
+families <- unique(sorted_data_tips_desc$family)
+number_of_families <- length(families)
+
+species_y <- 1:number_of_species / number_of_species
+names(species_y) <- sorted_data_tips_desc$label
+
+family_y <- 1:number_of_families / number_of_families
+names(family_y) <- families
+
+# code fgrid# code from https://stackoverflow.com/a/32100956/29281675
+curveMaker <- function(x1, y1, x2, y2, ...){
+  curve( plogis( x, scale = 0.08, loc = (x1 + x2) /2 ) * (y2-y1) + y1, 
+         x1, x2, add = TRUE, ...)
+}
+
+pdf(paste0(root, 'figures/figure_1_sp_family_connectors.pdf'), width = 4, height = 10)
+
+plot(NULL, xlim = c(0, 1), ylim = c(0, 1), axes = F, xlab = '', ylab = '')
+text(0.60, family_y, families, cex = 0.50, pos = 4)
 
 
+for ( i in 1:nrow(sorted_data_tips_desc)){ 
+  curveMaker(0.05, species_y[sorted_data_tips_desc[i, 'label']], 0.6, family_y[sorted_data_tips_desc[i, 'family']])
+}
 
+dev.off()
 
+#write.table(sorted_data_tips_desc$label, file = "labels_sorted.tsv", sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
-
-
-
-
-
+################################################################################
