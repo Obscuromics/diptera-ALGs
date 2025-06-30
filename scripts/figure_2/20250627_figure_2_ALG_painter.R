8f3d4bc4107bdfcf06bc5663318f09006ac9338a
aleksandrabliznina <ab66@sanger.ac.uk>
2025-06-30 13:55:13 +0100

    Updated scripts for Figure 2A,C,D,E

--- scripts/figure_2/20250627_figure_2_ALG_painter.R
+++ scripts/figure_2/20250627_figure_2_ALG_painter.R
@@ -1,4 +1,4 @@
-# Figure 2C - colour genomes with ALGs
+# Figure 2C - color given genomes with ALGs
 ################################################################################
 library(dplyr)
 library(ggplot2)
@@ -28,157 +28,66 @@ clean_data <- function(data) {
   data.frame(chrQ.y = cleaned_values)
 }
 ################################################################################
-
+root <- "/Users/ab66/Documents/sanger_work/diptera/diptera-ALGs/"
 ################################################################################
-n3 <- read.table("C:/Users/julia/Documents/Kamil/muller/minus26_89_n3.tsv", header = TRUE, sep = "\t")
-ALG_6 <- read.table("C:/Users/julia/Documents/Kamil/muller/small_dot_ALG.txt", header = TRUE, sep = "\t")
+# color palette
+pal <- c("M1" = "#1573afff", "M2" = "#e59d38ff", "M3" = "#f0e354ff", 
+         "M4" = "#169e73ff", "M5" = "#60b5e1ff", "M6" = "black", "100" = "white")
+################################################################################
+n3 <- read.table(paste0(root, "data/minus26_89_n3.tsv"), header = TRUE, sep = "\t")
+ALG_6 <- read.table(paste0(root, "data/small_dot_ALG.txt"), header = TRUE, sep = "\t")
 ALG_data <- rbind(n3, ALG_6)
 colnames(ALG_data) <- c("busco", "chrQ", "Qstart", "Qend")
-head(ALG_data)
 
-names_acc_fam_2 <- read.table("C:/Users/julia/Documents/Kamil/busco_fulltable/names_acc_fam_2.csv", sep = ",", header = TRUE)
+names_acc_fam_2 <- read.table(paste0(root, "data/names_acc_fam_2.csv"), sep = ",", header = TRUE)
 colnames(names_acc_fam_2) <- c("names", "num", "acc", "fam")
-head(names_acc_fam_2)
 
-directory <- "C:/Users/julia/Documents/Kamil/busco_fulltable/all_tol"
+directory <- paste0(root, "data/BUSCO_data/")
 new_colnames <- c("busco", "chrQ", "Qstart", "Qend")
 ################################################################################
-# create different busco_files and chrom_files list for plots
-# Version 1 - 4
-
-# Version 1
-# use specific genomes to illustrate small dot situation
-
-keep_small_d = names_acc_fam_2
-keep_small_d$busco_files <- paste0(keep_small_d$acc, ".tsv")
-keep_small_d$chrom_files <- paste0(keep_small_d$acc, "_chrominf.tsv")
-
-head(keep_small_d)
-
-spec_keep_dot <- c("Panorpa_germanica",
-                   "Lutzomyia_longipalpis",
-                   "Phlebotomus_papatasi",
-                   "Aedes_albopictus",
-                   "Tipula_confusa",
-                   "Bibio_marci",
-                   "Chrysops_caecutiens",
-                   "Anthrax_anthrax",
-                   "Bombylius_major",
-                   "Bombylius_discolor",
-                   "Villa_cingulata",
-                   "Drosophila_melanogaster")
-keep_filtered <- keep_small_d[keep_small_d$names %in% spec_keep_dot, ]
-head(keep_filtered)
-keep_filtered
-
-busco_files <- keep_filtered$busco_files
-chrom_files <- keep_filtered$chrom_files
-################################################################################
-# Version 2
-# use specific genomes to illustrate rearrangements
-
-keep_small_d = names_acc_fam_2
-keep_small_d$busco_files <- paste0(keep_small_d$acc, ".tsv")
-keep_small_d$chrom_files <- paste0(keep_small_d$acc, "_chrominf.tsv")
-
-head(keep_small_d)
-
-spec_keep_dot <- c("Topomyia_yanbarensis",
-                   "Aedes_albopictus",
-                   "Culex_pipiens",
-                   "Smittia_aterrima",
-                   "Ptychoptera_contaminata",
-                   "Ptychoptera_albimana",
-                   "Dilophus_febrilis",
-                   "Bibio_marci",
-                   "Empis_livida",
-                   "Drosophila_melanogaster")
-keep_filtered <- keep_small_d[keep_small_d$names %in% spec_keep_dot, ]
-head(keep_filtered)
-keep_filtered
-
-# change chironomidae and culicidae
-index_8 <- which(keep_filtered$num == 8)
-index_26 <- which(keep_filtered$num == 26)
-temp <- keep_filtered$num[index_8]
-keep_filtered$num[index_8] <- keep_filtered$num[index_26]
-keep_filtered$num[index_26] <- temp
-keep_filtered <- keep_filtered[order(keep_filtered$num), ]
-
-busco_files <- keep_filtered$busco_files
-chrom_files <- keep_filtered$chrom_files
-################################################################################
-# Version 3
-# plot removed genomes
-removed_genomes <- read.table("C:/Users/julia/Documents/Kamil/txt/removed.txt", 
-                              header = FALSE, sep = "\t", stringsAsFactors = FALSE)
-removed_genomes$V2 <- paste0(removed_genomes$V1, ".tsv")
-removed_genomes$V3 <- paste0(removed_genomes$V1, "_chrominf.tsv")
-busco_files <- removed_genomes$V2
-chrom_files <- removed_genomes$V3
-################################################################################
-# Version 4
 # Drosophilidae
 # Empididae
-# Bombyliidae
-# Syrphidae
-# Tipulidae
 # Bibionidae
 # Ptychopteridae
+# Chironomidae
 # Culicinae
 
 keep_small_d = names_acc_fam_2
 keep_small_d$busco_files <- paste0(keep_small_d$acc, ".tsv")
 keep_small_d$chrom_files <- paste0(keep_small_d$acc, "_chrominf.tsv")
 
-head(keep_small_d)
-
-spec_keep_dot <- c("Topomyia_yanbarensis",
-                   "Aedes_albopictus",
-                   "Culex_pipiens",
-                   "Ptychoptera_contaminata",
-                   "Ptychoptera_albimana",
+spec_keep_dot <- c("Drosophila_melanogaster",
+                   "Empis_livida",
                    "Dilophus_febrilis",
                    "Bibio_marci",
-                   "Tipula_confusa",
-                   "Volucella_inflata",
-                   "Anthrax_anthrax",
-                   "Bombylius_major",
-                   "Bombylius_discolor",
-                   "Villa_cingulata",
-                   "Empis_livida",
-                   "Drosophila_melanogaster")
+                   "Ptychoptera_contaminata",
+                   "Ptychoptera_albimana",
+                   "Chironomus_riparius",
+                   "Aedes_albopictus",
+                   "Culex_pipiens",
+                   "Topomyia_yanbarensis"
+                   #"Tipula_confusa",
+                   #"Bombylius_discolor",
+                   #"Bombylius_major",
+                   #"Anthrax_anthrax",
+                   #"Villa_cingulata"
+                   )
 
 keep_filtered <- keep_small_d[keep_small_d$names %in% spec_keep_dot, ]
-head(keep_filtered)
-keep_filtered
-
-# change chironomidae and culicidae
-#index_8 <- which(keep_filtered$num == 8)
-#index_26 <- which(keep_filtered$num == 26)
-#temp <- keep_filtered$num[index_8]
-#keep_filtered$num[index_8] <- keep_filtered$num[index_26]
-#keep_filtered$num[index_26] <- temp
-#keep_filtered <- keep_filtered[order(keep_filtered$num), ]
+keep_filtered <- keep_filtered[match(rev(spec_keep_dot), keep_filtered$names),]
 
 busco_files <- keep_filtered$busco_files
 chrom_files <- keep_filtered$chrom_files
 
-
-################################################################################
-# color palettes
-#pal <- c("M1" = "yellow4", "M2" = "springgreen3", "M3" = "#F3cdC7", 
-         #"M4" = "steelblue2", "M5" = "#FF7B9C", "M6" = "black", "100" = "white")
-
-pal <- c("M1" = "#1573afff", "M2" = "#e59d38ff", "M3" = "#f0e354ff", 
-         "M4" = "#169e73ff", "M5" = "#60b5e1ff", "M6" = "black", "100" = "white")
-################################################################################
 # plot
 plot_list <- list()
+
 for (j in 1:length(busco_files)) {
   
   busco_file_path <- file.path(directory, busco_files[j])
   chrom_file_path <- file.path(directory, chrom_files[j])
+  species <- keep_filtered$names[j]
+  family <- keep_filtered$fam[j]
   
   # Load the busco data
   busco_data <- read.table(busco_file_path, sep = "\t", header = FALSE, stringsAsFactors = FALSE)
@@ -197,7 +106,6 @@ for (j in 1:length(busco_files)) {
   
   # Sort data by order of chromosomes
   chr_order <- chrom_data$chr
-  chr_order
   busco_data_ALG_sorted <- busco_data_ALG[order(factor(busco_data_ALG$chrQ.x, levels = chr_order)), ]
   
   # Insert 80 empty BUSCOs between chromosomes
@@ -219,7 +127,6 @@ for (j in 1:length(busco_files)) {
     return(chr_data)
   })
   final_data <- do.call(rbind, flipped_data)
-  head(final_data)
   busco_data_ALG_sorted <- final_data
   
   # insert 80 empty BUSCOs between chromosomes
@@ -237,12 +144,9 @@ for (j in 1:length(busco_files)) {
   }
   result <- append(result, list(busco_data_ALG_sorted[nrow(busco_data_ALG_sorted), ]))
   busco_data_ALG_final <- do.call(rbind, result)
-  head(busco_data_ALG_final, 100)
-  tail(busco_data_ALG_final, 100)
   # remove everything but chrQ.y
   busco_data_ALG_final <- busco_data_ALG_final %>%
     select(-busco, -chrQ.x)
-  head(busco_data_ALG_final, 100)
   
   # clean edges of chromosomes
   cleaned_data <- clean_data(busco_data_ALG_final)
@@ -287,14 +191,32 @@ for (j in 1:length(busco_files)) {
       axis.ticks = element_blank(),
       legend.position = "none",
       panel.grid = element_blank()
-    )
+    ) +
+    labs(title = paste0(species, " (", family, ")"))
   
   # Append the plot to the plot_list
   plot_list[[j]] <- p
 }
+
 plot_list <- rev(plot_list)
-################################################################################
-grid.arrange(grobs = plot_list, ncol = 1)
-ggsave("trial.png", plot = grid.arrange(grobs = plot_list, ncol = 1), width = 10, height = 20)
+
+plt_all <- grid.arrange(grobs = plot_list, ncol = 1)
+ggsave(paste0(root, "figures/genomes_painted_by_ALGs.png"), plot = plt_all, dpi = 600)
+ggsave(paste0(root, "figures/genomes_painted_by_ALGs.svg"), plot = plt_all, dpi = 600)
 ################################################################################
 
+# Figure 2D - number of BUSCOs per ALG
+agg <- aggregate(ALG_data$busco, by = list(ALG_data$chrQ), FUN = length)
+colnames(agg) <- c("ALG", "n_markers")
+agg$ALG_label <- sub("M", "", agg$ALG)
+
+plt_n_markers <- ggplot(agg, aes(x = ALG_label, y = n_markers, fill = ALG)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = pal) +
+  labs(x = "ALG", y = "# of BUSCOs") +
+  theme_bw() +
+  theme(legend.position = "none")
+
+ggsave(paste0(root, "figures/n_buscos_per_ALGs.png"), plot = plt_n_markers, dpi = 600)
+ggsave(paste0(root, "figures/n_buscos_per_ALGs.svg"), plot = plt_n_markers, dpi = 600)
+################################################################################
