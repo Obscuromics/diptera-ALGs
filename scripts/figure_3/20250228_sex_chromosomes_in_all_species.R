3e6923f64d673ce3a3dc46b8e4d5c8d1d327df48
diekei <arif.maulana@evobio.eu>
2025-10-04 00:21:13 +0100

    Add new layout option for plot_sex_chrom

--- scripts/figure_3/20250228_sex_chromosomes_in_all_species.R
+++ scripts/figure_3/20250228_sex_chromosomes_in_all_species.R
@@ -96,9 +96,14 @@ plot_sex_chrom <- function(pdf_file,
                            subset_species      = NULL,
                            between_chr_gap_size = 0.1,
                            draw_borders = FALSE,
-                           border_col   = "black") {
-  
+                           border_col   = "black",
+                           multiX_layout = c("gapped","columns","stacked"),
+                           column_inner_margin = 0.02,
+                           repeat_x_axis_in_each_column = TRUE,
+                           draw_column_separators = FALSE,
+                           stacked_inner_gap = 0.05) {
   mode <- match.arg(mode)
+  multiX_layout <- match.arg(multiX_layout)
   
   ## ---------------- CONFIG ----------------
   pal   <- alg_pal
@@ -106,7 +111,6 @@ plot_sex_chrom <- function(pdf_file,
   
   ## ---------------- INPUT SUBSET (optional) ----------------
   species_to_plot <- sorted_data_tips_desc$label
-  
   if (!is.null(subset_species_file)) {
     subset_from_file <- tryCatch(
       scan(subset_species_file, what = character(), quiet = TRUE),
@@ -114,16 +118,11 @@ plot_sex_chrom <- function(pdf_file,
     )
     species_to_plot <- intersect(species_to_plot, subset_from_file)
   }
-  
   if (!is.null(subset_species)) {
     species_to_plot <- intersect(species_to_plot, subset_species)
   }
-  
   sdd <- subset(sorted_data_tips_desc, label %in% species_to_plot)
-  
-  if (nrow(sdd) == 0) {
-    stop("No species to plot after applying subset filters.")
-  }
+  if (nrow(sdd) == 0) stop("No species to plot after applying subset filters.")
   
   ## ---------------- HELPERS ----------------
   sum_d_cols <- function(df) {
@@ -133,7 +132,6 @@ plot_sex_chrom <- function(pdf_file,
     m[is.na(m)] <- 0
     colSums(m)
   }
-  
   norm <- function(x) tolower(trimws(x))
   multiX_set <- norm(multiX_species)
   labels_set <- norm(sdd$label)
@@ -143,46 +141,13 @@ plot_sex_chrom <- function(pdf_file,
             paste(not_found, collapse = ", "))
   }
   
-  ## ---------------- FAMILY INFO (for gaps and/or labels) ----------------
+  ## ---------------- FAMILY INFO ----------------
   if (family_gap > 0 || show_family_labels) {
     diptera_taxa <- read.csv("data/diptera/diptera_taxa.csv", header = TRUE)
-    sdd$family <- diptera_taxa$family[
-      match(sdd$label, diptera_taxa$species)
-    ]
+    sdd$family <- diptera_taxa$family[ match(sdd$label, diptera_taxa$species) ]
     sdd$family[is.na(sdd$family)] <- "Unknown"
   } else {
     sdd$family <- NA_character_
-  }
-  
-  ## ---------------- VERTICAL LAYOUT ----------------
-  number_of_species <- nrow(sdd)
-  
-  if (family_gap > 0) {
-    plot_species_order <- rev(sdd$label)
-    fam_vec_plot <- sdd$family[ match(plot_species_order, sdd$label) ]
-    
-    family_change_idx <- if (number_of_species > 1) {
-      which(fam_vec_plot[-1] != fam_vec_plot[-length(fam_vec_plot)])
-    } else integer(0)
-    
-    usable_height <- 1 - length(family_change_idx) * family_gap
-    if (usable_height <= 0) stop("family_gap too large for the number of family transitions.")
-    species_step <- usable_height / number_of_species
-    
-    y_mids <- numeric(number_of_species)
-    cursor <- species_step / 2
-    for (i in 1:number_of_species) {
-      y_mids[i] <- cursor
-      cursor <- cursor + species_step
-      if (i %in% family_change_idx) cursor <- cursor + family_gap
-    }
-    
-    species_y <- setNames(y_mids, plot_species_order)
-    
-  } else {
-    species_step <- (1 / number_of_species)
-    y_mids <- (1:number_of_species / number_of_species) - (species_step / 2)
-    species_y <- setNames(y_mids, rev(sdd$label))
   }
   
   ## ---------------- ABSOLUTE MODE PREPASS ----------------
@@ -194,7 +159,6 @@ plot_sex_chrom <- function(pdf_file,
       sp_tab <- all_genome_data[all_genome_data[, "species"] == species, , drop = FALSE]
       if (!any(sp_tab[, "X"])) next
       x_rows <- sp_tab[sp_tab[, "X"], , drop = FALSE]
-      
       spec_key <- norm(species)
       if (spec_key %in% multiX_set) {
         if (nrow(x_rows) > 0) {
@@ -209,85 +173,246 @@ plot_sex_chrom <- function(pdf_file,
     global_max <- if (length(bar_totals)) max(bar_totals) else 1
   }
   
-  ## ---------------- PLOT ----------------
-  pdf(pdf_file, height = pdf_height, width = pdf_width)
-  plot(NULL, xlim = c(0, 1), ylim = c(0, 1),
-       axes = FALSE, xlab = "", ylab = "")
+  ## ---------------- VERTICAL LAYOUT ----------------
+  plot_species_order <- rev(sdd$label)
+  sdd <- sdd[match(plot_species_order, sdd$label), , drop = FALSE]
+  number_of_species <- nrow(sdd)
   
-  for (i in 1:nrow(sdd)) {
-    species <- as.character(sdd[i, "label"])
-    sp_tab  <- all_genome_data[all_genome_data[, "species"] == species, ]
-    if (!any(sp_tab[, "X"])) next
-    
-    x_rows <- sp_tab[sp_tab[, "X"], , drop = FALSE]
-    
+  n_sub_rows <- integer(number_of_species)
+  chrom_lists <- vector("list", number_of_species)
+  
+  for (i in seq_len(number_of_species)) {
+    species <- as.character(sdd$label[i])
+    sp_tab  <- all_genome_data[all_genome_data[, "species"] == species, , drop = FALSE]
+    x_rows  <- if (nrow(sp_tab)) sp_tab[sp_tab[, "X"], , drop = FALSE] else sp_tab[FALSE, , drop = FALSE]
     spec_key <- norm(species)
-    if (spec_key %in% multiX_set) {
-      chrom_to_plot <- x_rows
+    
+    if (nrow(x_rows) == 0) {
+      chrom_lists[[i]] <- x_rows[FALSE, , drop = FALSE]
+      n_sub_rows[i] <- 1
+    } else if (multiX_layout == "stacked" && (spec_key %in% multiX_set)) {
+      chrom_lists[[i]] <- x_rows
+      n_sub_rows[i] <- nrow(x_rows)
     } else {
       merged_vals <- sum_d_cols(x_rows)
-      chrom_to_plot <- x_rows[1, , drop = FALSE]
-      chrom_to_plot[1, d_cols] <- merged_vals
-      chrom_to_plot <- chrom_to_plot[1, , drop = FALSE]
+      one <- x_rows[1, , drop = FALSE]
+      one[1, d_cols] <- merged_vals
+      chrom_lists[[i]] <- one
+      n_sub_rows[i] <- 1
     }
-    
-    chr_number <- nrow(chrom_to_plot)
-    y_mid <- species_y[species]
-    y_bot <- y_mid - (species_step / 2)
-    y_top <- y_mid + (species_step / 2)
-    
-    gap_x <- between_chr_gap_size
-    total_bar_width <- max(1 - max(chr_number - 1, 0) * gap_x, 0)
-    if (total_bar_width == 0 && chr_number > 0) {
-      gap_x <- (1 - 0.02) / max(chr_number - 1, 1)
-      total_bar_width <- 1 - (chr_number - 1) * gap_x
+  }
+  
+  if (multiX_layout != "stacked") {
+    if (family_gap > 0) {
+      fam_vec_plot <- sdd$family
+      family_change_idx <- if (number_of_species > 1) which(fam_vec_plot[-1] != fam_vec_plot[-number_of_species]) else integer(0)
+      usable_height <- 1 - length(family_change_idx) * family_gap
+      if (usable_height <= 0) stop("family_gap too large for the number of family transitions.")
+      species_step <- usable_height / number_of_species
+      y_mids <- numeric(number_of_species)
+      cursor <- species_step / 2
+      for (i in 1:number_of_species) {
+        y_mids[i] <- cursor
+        cursor <- cursor + species_step
+        if (i %in% family_change_idx) cursor <- cursor + family_gap
+      }
+      species_y <- setNames(y_mids, sdd$label)
+    } else {
+      species_step <- (1 / number_of_species)
+      y_mids <- (1:number_of_species / number_of_species) - (species_step / 2)
+      species_y <- setNames(y_mids, sdd$label)
     }
-    per_bar_width <- if (chr_number > 0) total_bar_width / chr_number else 0
+  } else {
+    fam_vec_plot <- sdd$family
+    family_change_idx <- if (number_of_species > 1) which(fam_vec_plot[-1] != fam_vec_plot[-number_of_species]) else integer(0)
     
-    for (k in 1:chr_number) {
-      x_left  <- (k - 1) * (per_bar_width + gap_x)
-      x_right <- x_left + per_bar_width
+    usable_height <- 1 - length(family_change_idx) * family_gap
+    if (usable_height <= 0) stop("family_gap too large for the number of family transitions.")
+    
+    total_units <- sum(n_sub_rows)
+    if (total_units == 0) stop("No X chromosomes to plot.")
+    unit_h <- usable_height / total_units
+    
+    bar_h <- unit_h * (1 - stacked_inner_gap)
+    gap_h <- unit_h * stacked_inner_gap
+    
+    species_block_mid <- numeric(number_of_species)
+    
+    subrow_bounds <- vector("list", number_of_species)
+    cursor <- 0
+    for (i in 1:number_of_species) {
+      k <- n_sub_rows[i]
+      if (k == 0) { subrow_bounds[[i]] <- matrix(numeric(0), ncol = 2); next }
+      starts <- cursor + (0:(k-1)) * unit_h
+      y_bots <- starts + gap_h/2
+      y_tops <- y_bots + bar_h
+      subrow_bounds[[i]] <- cbind(y_bots, y_tops)
       
-      vals <- as.numeric(chrom_to_plot[k, d_cols, drop = TRUE])
-      vals[is.na(vals)] <- 0
-      s <- sum(vals)
-      if (s <= 0) next
+      species_block_mid[i] <- cursor + (k * unit_h) / 2
+      cursor <- cursor + k * unit_h
+      if (i %in% family_change_idx) cursor <- cursor + family_gap
+    }
+    species_y <- setNames(species_block_mid, sdd$label)
+  }
+  
+  ## ---------------- COLUMNS LAYOUT COUNT (for columns only) ----------------
+  n_cols <- 1
+  if (multiX_layout == "columns") {
+    max_chr <- 1
+    for (i in 1:number_of_species) {
+      max_chr <- max(max_chr, nrow(chrom_lists[[i]]))
+    }
+    n_cols <- max_chr
+  }
+  
+  ## ---------------- PLOT ----------------
+  xlim <- if (multiX_layout == "columns") c(0, n_cols) else c(0, 1)
+  pdf(pdf_file, height = pdf_height, width = pdf_width)
+  plot(NULL, xlim = xlim, ylim = c(0, 1), axes = FALSE, xlab = "", ylab = "")
+  
+  if (multiX_layout == "columns" && draw_column_separators && n_cols > 1) {
+    abline(v = 0:n_cols, col = "grey90", lwd = 1)
+  }
+  
+  ## ---------------- DRAW ----------------
+  for (i in 1:number_of_species) {
+    species <- as.character(sdd$label[i])
+    chrom_to_plot <- chrom_lists[[i]]
+    
+    if (nrow(chrom_to_plot) == 0) next
+    
+    if (multiX_layout == "gapped" || multiX_layout == "columns") {
+      if (multiX_layout == "gapped") {
+        chr_number <- nrow(chrom_to_plot)
+        y_mid <- species_y[species]
+        species_step <- (1 / number_of_species)
+      }
       
-      if (mode == "relative") {
-        edges01 <- c(0, cumsum(vals) / s)
-        edges   <- x_left + (x_right - x_left) * edges01
-        for (j in 1:6) rect(edges[j], y_bot, edges[j + 1], y_top, col = pal[j], border = if (draw_borders) border_col else NA)
-      } else {
-        cum_abs <- c(0, cumsum(vals)) / global_max
-        edges_abs <- x_left + pmin(cum_abs, 1) * per_bar_width
-        for (j in 1:6) rect(edges_abs[j], y_bot, edges_abs[j + 1], y_top, col = pal[j], border = if (draw_borders) border_col else NA)
+      if (multiX_layout == "gapped") {
+        chr_number <- nrow(chrom_to_plot)
+        y_mid <- species_y[species]
+        if (!exists("species_step")) {
+          species_step <- 1 / number_of_species
+        }
+        y_bot <- y_mid - (species_step / 2)
+        y_top <- y_mid + (species_step / 2)
+        
+        gap_x <- between_chr_gap_size
+        total_bar_width <- max(1 - max(chr_number - 1, 0) * gap_x, 0)
+        if (total_bar_width == 0 && chr_number > 0) {
+          gap_x <- (1 - 0.02) / max(chr_number - 1, 1)
+          total_bar_width <- 1 - (chr_number - 1) * gap_x
+        }
+        per_bar_width <- if (chr_number > 0) total_bar_width / chr_number else 0
+        
+        for (k in 1:chr_number) {
+          x_left  <- (k - 1) * (per_bar_width + gap_x)
+          x_right <- x_left + per_bar_width
+          vals <- as.numeric(chrom_to_plot[k, d_cols, drop = TRUE]); vals[is.na(vals)] <- 0
+          s <- sum(vals); if (s <= 0) next
+          if (mode == "relative") {
+            edges01 <- c(0, cumsum(vals) / s)
+            edges   <- x_left + (x_right - x_left) * edges01
+            for (j in 1:6) rect(edges[j], y_bot, edges[j + 1], y_top, col = pal[j],
+                                border = if (draw_borders) border_col else NA)
+          } else {
+            cum_abs <- c(0, cumsum(vals)) / global_max
+            edges_abs <- x_left + pmin(cum_abs, 1) * (x_right - x_left)
+            for (j in 1:6) rect(edges_abs[j], y_bot, edges_abs[j + 1], y_top, col = pal[j],
+                                border = if (draw_borders) border_col else NA)
+          }
+        }
+      } else if (multiX_layout == "columns") {
+        chr_number <- nrow(chrom_to_plot)
+        y_mid <- species_y[species]
+        if (!exists("species_step")) species_step <- 1 / number_of_species
+        y_bot <- y_mid - (species_step / 2)
+        y_top <- y_mid + (species_step / 2)
+        
+        for (k in 1:chr_number) {
+          x_left  <- (k - 1) + column_inner_margin
+          x_right <-  k      - column_inner_margin
+          vals <- as.numeric(chrom_to_plot[k, d_cols, drop = TRUE]); vals[is.na(vals)] <- 0
+          s <- sum(vals); if (s <= 0) next
+          if (mode == "relative") {
+            edges01 <- c(0, cumsum(vals) / s)
+            edges   <- x_left + (x_right - x_left) * edges01
+            for (j in 1:6) rect(edges[j], y_bot, edges[j + 1], y_top, col = pal[j],
+                                border = if (draw_borders) border_col else NA)
+          } else {
+            cum_abs <- c(0, cumsum(vals)) / global_max
+            edges_abs <- x_left + pmin(cum_abs, 1) * (x_right - x_left)
+            for (j in 1:6) rect(edges_abs[j], y_bot, edges_abs[j + 1], y_top, col = pal[j],
+                                border = if (draw_borders) border_col else NA)
+          }
+        }
+      }
+      
+    } else if (multiX_layout == "stacked") {
+      bounds <- subrow_bounds[[i]]
+      for (k in seq_len(nrow(bounds))) {
+        y_bot <- bounds[k, 1]; y_top <- bounds[k, 2]
+        
+        if (k > nrow(chrom_to_plot)) next
+        
+        vals <- as.numeric(chrom_to_plot[k, d_cols, drop = TRUE]); vals[is.na(vals)] <- 0
+        s <- sum(vals); if (s <= 0) next
+        
+        if (mode == "relative") {
+          edges01 <- c(0, cumsum(vals) / s)
+          for (j in 1:6) rect(edges01[j], y_bot, edges01[j + 1], y_top, col = pal[j],
+                              border = if (draw_borders) border_col else NA)
+        } else {
+          cum_abs <- c(0, cumsum(vals)) / global_max
+          for (j in 1:6) rect(cum_abs[j], y_bot, cum_abs[j + 1], y_top, col = pal[j],
+                              border = if (draw_borders) border_col else NA)
+        }
       }
     }
     
     ## ---------- OPTIONAL LABELS ----------
     if (show_species_labels) {
-      text(x = 0 - label_offset, y = y_mid, labels = species,
+      text(x = xlim[1] - label_offset, y = species_y[species], labels = species,
            xpd = TRUE, adj = 1, cex = label_cex)
     }
     if (show_family_labels && !is.na(sdd$family[i])) {
-      text(x = 1 + label_offset, y = y_mid, labels = sdd$family[i],
+      text(x = xlim[2] + label_offset, y = species_y[species], labels = sdd$family[i],
            xpd = TRUE, adj = 0, cex = label_cex)
     }
   }
-
-    ## ---------------- AXIS ----------------
-  if (mode == "relative") {
-    # 0 to 1 → percentage
-    axis(1, at = seq(0, 1, by = 0.2),
-         labels = paste0(seq(0, 100, by = 20), "%"),
-         cex.axis = 0.7, line = 0)
+  
+  ## ---------------- AXIS ----------------
+  if (multiX_layout == "columns") {
+    usr <- par("usr"); y0 <- usr[3]; tick_len <- strheight("M") * 0.35
+    draw_col_axis <- function(x0, x1, at01, labels) {
+      segments(x0, y0, x1, y0, xpd = NA)
+      xs <- x0 + (x1 - x0) * at01
+      segments(xs, y0, xs, y0 - tick_len, xpd = NA)
+      text(xs, y0 - tick_len*2.5, labels = labels, xpd = NA, cex = 0.7)
+    }
+    if (mode == "relative") {
+      at01 <- seq(0, 1, by = 0.2); labs <- paste0(seq(0, 100, by = 20), "%")
+    } else {
+      pretty_breaks <- pretty(c(0, global_max), n = 5)
+      at01 <- pretty_breaks / max(global_max, .Machine$double.eps); labs <- pretty_breaks
+    }
+    for (col in 1:n_cols) {
+      x0 <- (col - 1) + column_inner_margin
+      x1 <-  col      - column_inner_margin
+      draw_col_axis(x0, x1, at01, labs)
+    }
+    
   } else {
-    # absolute → scale using global_max
-    # Decide a nice tick step
-    pretty_breaks <- pretty(c(0, global_max), n = 5)
-    axis(1, at = pretty_breaks / global_max,
-         labels = pretty_breaks,
-         cex.axis = 0.7, line = 0)
+    if (mode == "relative") {
+      axis(1, at = seq(0, 1, by = 0.2),
+           labels = paste0(seq(0, 100, by = 20), "%"),
+           cex.axis = 0.7, line = 0)
+    } else {
+      pretty_breaks <- pretty(c(0, global_max), n = 5)
+      axis(1, at = pretty_breaks / global_max,
+           labels = pretty_breaks,
+           cex.axis = 0.7, line = 0)
+    }
   }
   
   dev.off()
@@ -356,6 +481,25 @@ plot_sex_chrom(
   multiX_species = c("Dolichopus_virgultorum", "Sicus_ferrugineus"),
   draw_borders = TRUE,
   border_col   = "black"
+)
+
+# Full dataset, relative newest version
+plot_sex_chrom(
+  pdf_file = "sex_chrom_base_relative_v3.pdf",
+  mode = "relative",
+  #family_gap = 0.001,
+  #subset_species_file = "data/diptera/diptera_taxa_selected.txt",
+  pdf_height = 26, pdf_width = 3,
+  show_species_labels = TRUE,
+  show_family_labels  = FALSE,
+  #label_cex = 0.3,
+  #between_chr_gap_size = 0.1,
+  multiX_species = c("Dolichopus_virgultorum", "Sicus_ferrugineus"),
+  draw_borders = TRUE,
+  border_col   = "black",
+  multiX_layout = "stacked"
+  #repeat_x_axis_in_each_column = TRUE,
+  #stacked_inner_gap = 0.08
 )
 
 ############################################
