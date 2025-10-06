dbc96e7f9bcecf1054f154f6e5275cd7b2078ed6
KamilSJaron <kamiljaron@gmail.com>
2025-10-06 11:30:57 +0200

    adding script of Chromosomes vs Alg overview

--- scripts/20250620_colour_pal.R
+++ scripts/20250620_colour_pal.R
@@ -4,7 +4,17 @@ pal <- c("d1" = "#169e73ff", "d2" = "#e59d38ff", "d3" = "#1573afff",
          "db2a" = "#ffbd60ff", "db2b" = "#f29717ff", 
          "db3a" = "#1591dfff", "db3b" = "#005990ff",
          "db4a" = "#f0e354ff", "db4b" = "#e2d119ff",
-         "db5" =  "#60b5e1ff", "db6" = "black")
+         "db5" =  "#60b5e1ff", "db6" = "black",
+         "ds1" = "#008d17ff", "ds2" = "#60b5e1ff", "ds3" = "#988900ff",
+         "ds4" = "#faed00ff", "ds5" = "#88ca91ff", "ds6" = "black")
+
+# NC_004354.4 - X - A
+# NT_033779.5 - 2L - B
+# NT_033778.4 - 2R - C
+# NT_037436.4 - 3L - D
+# NT_033777.3 - 3R - E
+# NC_004353.4 - 4 - F
+# NC_024512.1 - Y
 
 # pal <- c("d1" = "#1573afff", "d5" = "#e59d38ff", "d3" = "#f0e354ff",
 #          "d4" = "#169e73ff", "d2" = "#60b5e1ff", "d6" = "black", "100" = "white")
---
+++ scripts/table_of_chromosomes_vs_ancestry.R
@@ -0,0 +1,29 @@
+
+suppressPackageStartupMessages(library(argparse))
+
+parser <- ArgumentParser()
+
+parser$add_argument("-a", "-alg", 
+    help="The name of the ALG definitions to be used.")
+parser$add_argument("-s", "-species", 
+    help="Species to be tabularised.")
+
+args <- parser$parse_args()
+
+source('scripts/20250620_colour_pal.R')
+
+# "Atylotus_latistriatus.syngraph.buscos.tsv"
+species_file <- paste0('data/busco_tables/',  args$s, '.syngraph.buscos.tsv')
+species_file_y <- paste0('data/busco_tables/',  args$s, '.syngraph.y.buscos.tsv')
+
+buscos <- rbind(read.table(species_file, sep = '\t'), read.table(species_file_y, sep = '\t'))
+ALGs <- read.table(args$a)
+
+row.names(ALGs) <- ALGs[, 1]
+
+buscos[, 'ALG'] <- ALGs[buscos[, 1], 2]
+
+sum(is.na(buscos[, 'ALG'])) # filt BUSCOs wo ALG assignment 
+buscos <- buscos[!is.na(buscos[, 'ALG']), ]
+
+table(buscos[, 2], buscos[, 'ALG'])
